c9c9b7082a89b3d6ff3e72c2a68c8702b3ec63fa
Marianych <maryanich.sergey@gmail.com>
2020-02-06 15:03:39 +0200

    forward to redirect

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -9,6 +9,7 @@ public class UserServlet extends HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-request.getRequestDispatcher("/users.jsp").forward(request,response);
+//request.getRequestDispatcher("/users.jsp").forward(request,response);
+        response.sendRedirect("users.jsp");
     }
 }
