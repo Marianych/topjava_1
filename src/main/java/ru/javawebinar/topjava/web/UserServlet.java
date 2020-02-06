bb9cb17b500be35b2e53a6c03221f7dc902ecfe2
Marianych <maryanich.sergey@gmail.com>
2020-02-06 16:59:36 +0200

    logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,14 +1,20 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        LOG.debug("redirect to user list");
 //request.getRequestDispatcher("/users.jsp").forward(request,response);
         response.sendRedirect("users.jsp");
     }
