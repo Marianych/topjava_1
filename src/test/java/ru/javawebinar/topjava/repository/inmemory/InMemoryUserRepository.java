f8d54de046c7c6c6bf01faa87801bcdcd5260d3f
Marianych <maryanich.sergey@gmail.com>
2020-02-09 17:24:17 +0200

    add JUNIT test (example)

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,19 +3,23 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.web.UserTestData;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.web.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.web.UserTestData.USER;
 
 @Repository
 public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
-    public static final int USER_ID = 1;
-    public static final int ADMIN_ID = 2;
+
+    public void init() {
+        map.clear();
+        map.put(UserTestData.USER_ID, USER);
+        map.put(UserTestData.ADMIN_ID, ADMIN);
+    }
 
     @Override
     public List<User> getAll() {
---
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -0,0 +1,72 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.junit.*;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.UserTestData;
+
+import javax.jws.soap.SOAPBinding;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.web.UserTestData.ADMIN;
+
+public class InMemoryAdminRestControllerTest {
+    private static ConfigurableApplicationContext appCtx;
+    private static AdminRestController controller;
+
+
+    @BeforeClass
+    public static void beforeClass() {
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
+        controller = appCtx.getBean(AdminRestController.class);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        appCtx.close();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        InMemoryUserRepository repository = appCtx.getBean(InMemoryUserRepository.class);
+        repository.init();
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void delete() throws Exception {
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
+        Assert.assertEquals(users.size(), 1);
+        Assert.assertEquals(users.iterator().next(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deleteNotFound() throws Exception{
+        controller.delete(20);
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getByMail() {
+    }
+}
