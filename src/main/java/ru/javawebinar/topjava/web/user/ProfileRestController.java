46d344ef5814617991268cee254ac40078ef28f5
Marianych <maryanich.sergey@gmail.com>
2020-02-14 12:53:39 +0200

    minor clean

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.web.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.User;
 
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,8 +1,6 @@
 package ru.javawebinar.topjava;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
